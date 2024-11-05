70e4c9db546f927431e635fd3ecf6005e836f1ac
황상윤 <s_infinite@naver.com>
2024-11-05 19:38:20 +0900

    [feat] main login signup 링크 연결

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function LoginPage() {
-	return <div>LoginPage</div>;
+	return (
+		<div>
+			<h1>로그인</h1>
+			<Link to="/signup">회원 가입</Link>
+		</div>
+	);
 }
